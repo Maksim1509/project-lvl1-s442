fe0a527a86b5c696d3c5facbc635d087f55d381d
Maksim1509 <maaa23@mail.ru>
2019-02-05 14:52:49 +0400

    add brain-evan

--- src/index.js
+++ src/index.js
@@ -5,4 +5,38 @@ const greeting = () => {
   const name = readlineSync.question('May i have your name? ');
   console.log(`Hello ${name}!`);
 };
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const isEven = int => int % 2 === 0;
+const sayYesOrNo = int => (isEven(int) ? 'yes' : 'no');
+
+const game = (name, acc) => {
+  const int = getRandomInt(1, 100);
+  const correctAnswer = sayYesOrNo(int);
+  if (acc === 3) {
+    console.log(`Congratulations, ${name}!`);
+    return true;
+  }
+  console.log(`Question: ${int}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+  } if (answer !== correctAnswer) {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".
+        Let's try again, ${name}!`);
+  }
+
+  acc = answer === sayYesOrNo(int) ? acc + 1 : acc;
+  return game(name, acc);
+};
+
+export const gameEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const name = readlineSync.question('May i have your name? ');
+  console.log(`Hello ${name}!`);
+
+  return game(name, 0);
+};
+
 export default greeting;
