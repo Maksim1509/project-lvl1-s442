a6f5813f4d0f099a54c29c5bd8301a657c07bbf2
Maksim1509 <maaa23@mail.ru>
2019-02-08 11:25:53 +0400

    correction

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,25 +1,23 @@
 import { startGame } from '..';
 import getRandomInt from '../utils';
 
-
-const primeNum = 1;
-
-const isPrimeNum = (num) => {
-  const iter = (divisibleNum, divisor) => {
+const isPrime = (num) => {
+  const primeNum = 1;
+  const iter = (divisor) => {
     if (divisor === primeNum) {
       return true;
-    } if (divisibleNum % divisor === 0) {
+    } if (num % divisor === 0) {
       return false;
     }
-    return iter(divisibleNum, divisor - 1);
+    return iter(divisor - 1);
   };
-  return iter(num, Math.floor(num / 2));
+  return iter(Math.floor(num / 2));
 };
 
 const getDataForGame = () => {
   const numForQuestion = getRandomInt(1, 100);
   const question = `Question: ${numForQuestion}`;
-  const correctAnswer = isPrimeNum(numForQuestion) ? 'yes' : 'no';
+  const correctAnswer = isPrime(numForQuestion) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
